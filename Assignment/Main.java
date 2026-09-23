fa937f1e14e77206d8a94fed9373bdd74889f354
SeanTheKiller <xxsaurexxx21@gmail.com>
2026-09-23 10:34:50 +0800

    GC

--- Assignment/Main.java
+++ Assignment/Main.java
@@ -1,7 +1,15 @@
+import javax.swing.SwingUtilities;
+import login.LoginFrame;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World Jing jing jing jing heng is hensome");
+
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame loginFrame = new LoginFrame();
+
+            loginFrame.setLocationRelativeTo(null);
+            loginFrame.setVisible(true);
+        });
     }
 }
-
-
